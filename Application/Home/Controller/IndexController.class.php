85827b32344d64f4dc85cdca56348a45f9431c05
Honlan <493722771@qq.com>
2015-06-15 17:57:44 +0800

    web stat

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,11 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$ip = get_client_ip();
+    	$ts = time();
+    	if(!M('pv')->where('ip = "'.$ip.'" AND ts > '.($ts - 3600 * 3))->count()){
+    		M('pv')->data(array('ip'=>$ip, 'ts'=>$ts))->add();
+    	}
     	$this->display();
     }
 }
--- Application/Home/Controller/WebController.class.php
+++ Application/Home/Controller/WebController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class WebController extends Controller {
     public function index(){
+    	$this->pv = M('pv')->count();
     	$this->display();
     }
 }
