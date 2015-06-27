aa5036ee26548fd375b210fe436690cb8218cb4c
Honlan <493722771@qq.com>
2015-06-27 22:44:26 +0800

    finish wifi timezone distributions

--- Application/Home/Controller/AffairController.class.php
+++ Application/Home/Controller/AffairController.class.php
@@ -3,9 +3,24 @@ namespace Home\Controller;
 use Think\Controller;
 class AffairController extends Controller {
     public function index(){
-    	$this->tags = M('tag_type_boy')->select();
+    	$this->tags = M('tag_type_boy')->field('tag')->select();
     	$this->tagTypeBoy = json_encode(M('tag_type_boy')->select());
     	$this->tagTypeGirl = json_encode(M('tag_type_girl')->select());
+    	
+    	$trafficTimezone = M('traffic_timezone')->select();
+    	foreach ($trafficTimezone as $key => $value) {
+    		$data = array();
+    		$data[] = $value['place'];
+    		foreach ($value as $k => $v) {
+    			if ($k == 'degree' || $k == 'place') continue;
+    			else {
+    				$data[] = intval($v);
+    			}
+    		}
+    		$temp[$value['degree']][$value['place']] = $data;
+    	}
+    	$this->trafficTimezone = json_encode($temp);
+
     	$this->display();
     }
 }
--- sql/pv.sql
+++ sql/pv.sql
@@ -1,64 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.2.5
--- http://www.phpmyadmin.net
---
--- Host: localhost:8889
--- Generation Time: Jun 24, 2015 at 06:23 AM
--- Server version: 5.5.38
--- PHP Version: 5.5.14
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Source Server         : myMac
+ Source Server Type    : MySQL
+ Source Server Version : 50538
+ Source Host           : localhost
+ Source Database       : elite
 
---
--- Database: `elite`
---
+ Target Server Type    : MySQL
+ Target Server Version : 50538
+ File Encoding         : utf-8
 
--- --------------------------------------------------------
+ Date: 06/27/2015 22:43:31 PM
+*/
 
---
--- Table structure for table `pv`
---
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `pv`
+-- ----------------------------
 DROP TABLE IF EXISTS `pv`;
 CREATE TABLE `pv` (
-`ID` int(11) NOT NULL,
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(20) NOT NULL,
-  `ts` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+  `ts` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `pv`
---
+-- ----------------------------
+--  Records of `pv`
+-- ----------------------------
+BEGIN;
+INSERT INTO `pv` VALUES ('1', '0.0.0.0', '1434361635'), ('2', '0.0.0.0', '1434362064'), ('3', '0.0.0.0', '1434374881'), ('4', '0.0.0.0', '1434416966'), ('5', '0.0.0.0', '1434428805'), ('6', '0.0.0.0', '1434466164'), ('7', '0.0.0.0', '1434505447'), ('8', '0.0.0.0', '1434550673'), ('9', '0.0.0.0', '1434632845'), ('10', '0.0.0.0', '1434695854'), ('11', '0.0.0.0', '1435119654'), ('12', '0.0.0.0', '1435195692'), ('13', '0.0.0.0', '1435323461'), ('14', '0.0.0.0', '1435408204');
+COMMIT;
 
-INSERT INTO `pv` (`ID`, `ip`, `ts`) VALUES
-(1, '0.0.0.0', 1434361635),
-(2, '0.0.0.0', 1434362064),
-(3, '0.0.0.0', 1434374881),
-(4, '0.0.0.0', 1434416966),
-(5, '0.0.0.0', 1434428805),
-(6, '0.0.0.0', 1434466164),
-(7, '0.0.0.0', 1434505447),
-(8, '0.0.0.0', 1434550673),
-(9, '0.0.0.0', 1434632845),
-(10, '0.0.0.0', 1434695854),
-(11, '0.0.0.0', 1435119654);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `pv`
---
-ALTER TABLE `pv`
- ADD PRIMARY KEY (`ID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `pv`
---
-ALTER TABLE `pv`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+SET FOREIGN_KEY_CHECKS = 1;
