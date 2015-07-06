49b9c1b3b3f6eeb5ac0ad8464150e2b2255a0ece
jjdblast <jjdblast@gmail.com>
2015-07-07 03:05:45 +0800

    Index predict done

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -39,7 +39,10 @@ class IndexController extends Controller {
     }
 
     // 获取地图数据
-    public function mensa_status($query, $type){
+    public function mensa_status(){
+        $query = $_POST['query'];
+        $type = $_POST['type'];
+
 		$timeformat = "%Y-%m-%d %X";
 		$Mensa = M('mensa'); // 实例化Mensa对象
         //$Mensa = M('oneday'); // 实例化oneday对象
@@ -58,8 +61,35 @@ class IndexController extends Controller {
         echo $data1;
     }
     
-    // 获取各食堂分布数据
-    public function sum_amount($query){
+    // 获取各食堂消费数据
+    public function mensa_amount(){
+        // 传参
+        $query = $_POST['query'];
+
+        $timeformat = "%Y-%m-%d %X";
+        $Mensa = M('mensa'); // 实例化Mensa对象
+        //$Mensa = M('oneday'); // 实例化oneday对象
+        // 数据初始化时间
+        $stamp0 = "2014-11-03 05:00:00";
+        $stamp1 = "2014-11-03 05:01:00";
+        // 时间的查询条件
+        $time_now = strftime($timeformat ,DateAdd("n" , $query, strtotime($stamp0))); // 从初始时刻开始，增加$query分钟
+        // 复杂语句直接query, 拼接字符串用黑点.
+        $select_header = "select toacount, accountname, SUM(amount) as 'sales' from  mensa_name where times <";
+        $select_time = "'".$time_now."'";
+        $select_group = " GROUP BY toacount ORDER BY SUM(amount) DESC";
+        $select_query = $select_header.$select_time.$select_group;
+        //echo $select_query;
+        $data = $Mensa->query($select_query);
+        $data1 = json_encode($data);
+        echo $data1;
+    }    
+
+    // 获取消费总额数据
+    public function sum_amount(){
+        // 传参
+        $query = $_POST['query'];
+
 		$timeformat = "%Y-%m-%d %X";
         $Mensa = M('mensa'); // 实例化Mensa对象
         //$Mensa = M('oneday'); // 实例化oneday对象
@@ -77,25 +107,42 @@ class IndexController extends Controller {
         echo $data1;
     }      
     
-    // 获取消费总额数据
-    public function mensa_amount($query){
-		$timeformat = "%Y-%m-%d %X";
-        $Mensa = M('mensa'); // 实例化Mensa对象
-        //$Mensa = M('oneday'); // 实例化oneday对象
-        // 数据初始化时间
-        $stamp0 = "2014-11-03 05:00:00";
-        $stamp1 = "2014-11-03 05:01:00";
-        // 时间的查询条件
-        $time_now = strftime($timeformat ,DateAdd("n" , $query, strtotime($stamp0))); // 从初始时刻开始，增加$query分钟
+    // 预测食堂人流量
+    public function mensa_predict(){
+        // 传参
+        $query = $_GET['query'];
+        //$mensa = $_GET['mensa'];
+        $mensa = $_GET['mensa'];        
+        // 边界条件
+        // query是从00:00:00开始的分钟数,减去偏移量300
+        // 预测的slice是5min,故还需除以5
+        $tidx = ceil(($query-300)/5);
+        // 移动窗口的单侧宽度为24*5=120min,所以上区间是180,下区间是24
+        if ($tidx>180){
+            $tlow = 204-47;
+            $thigh = 204;
+        }
+        elseif ($tidx<25) {
+            $tlow = 1;
+            $thigh = 1+47;        
+        }
+        else{
+            $tlow = $tidx-24; // 预测窗口23，历史窗口24，加上本身，窗口宽度共48位
+            $thigh = $tidx+23;   
+        }
+        $windows_size = 48;
+        // 取数据sql语句
+        // select minhang1 from predict_ratios limit 10,48 #返回第10行数据(含)后的48条数据
+        $Ratios = M('predict_ratios'); // 实例化predict_ratios对象
         // 复杂语句直接query, 拼接字符串用黑点.
-        $select_header = "select toacount, accountname, SUM(amount) as 'sales' from  mensa_name where times <";
-        $select_time = "'".$time_now."'";
-        $select_group = " GROUP BY toacount ORDER BY SUM(amount) DESC";
-        $select_query = $select_header.$select_time.$select_group;
-        //echo $select_query;
-        $data = $Mensa->query($select_query);
+        $select_header = "select ";
+        $select_mensa =  "$mensa"." from predict_ratios limit ";
+        $select_windows = "$tlow".", "."$windows_size";
+        $select_query = $select_header.$select_mensa.$select_windows;
+        // echo $select_query;
+        $data = $Ratios->query($select_query);
         $data1 = json_encode($data);
         echo $data1;
-    }
+    }   
 
 }
