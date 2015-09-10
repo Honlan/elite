a7a93e075de32698311f372f47a4d14c88a1103d
Honlan <493722771@qq.com>
2015-09-10 15:47:59 +0800

    auto fill student id

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -3,6 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class CommonController extends Controller{
 	public function _initialize(){
+		session('elite_uid', '66183190');
 		if(!isset($_SESSION['elite_uid'])){
 			$this->redirect('Index/index');
 		}
